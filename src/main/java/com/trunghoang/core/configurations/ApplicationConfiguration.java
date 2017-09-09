fd1d219a18e3e391ea1eec988524f531875b4cf3
Trung Hoang <hoangminhtrung1995@gmail.com>
2017-09-09 21:30:33 +0700

    Optimize scan packages

--- src/main/java/com/trunghoang/core/configurations/ApplicationConfiguration.java
+++ src/main/java/com/trunghoang/core/configurations/ApplicationConfiguration.java
@@ -12,7 +12,11 @@ import org.springframework.context.annotation.Import;
  *
  */
 @Configuration
-@ComponentScan(basePackages = { "com.trunghoang" })
+@ComponentScan(basePackages = { "com.trunghoang.business.repositories",
+		"com.trunghoang.business.repositories.impl",
+		"com.trunghoang.business.services",
+		"com.trunghoang.business.services.impl",
+		"com.trunghoang.core.controllers" })
 @Import({ DatabaseConfiguration.class, EntityConfiguration.class })
 public class ApplicationConfiguration {
 	private static Logger logger = LoggerFactory
